2ee060e46a28dceb135a1e769cd73db6d8e15d99
Nelson Neto <nelson.ash@outlook.com>
2023-11-14 17:48:31 +0000

    add service validator

--- src/Salddus.Api/Program.cs
+++ src/Salddus.Api/Program.cs
@@ -1,10 +1,15 @@
 using FluentValidation;
+using System.Reflection;
 using Salddus.Api.Features;
 
 var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(options =>
+{
+    var assemblyName = Assembly.GetExecutingAssembly().FullName?.Split(',')[0];
+    options.IncludeXmlComments(Path.Combine(AppContext.BaseDirectory, $"{assemblyName}.xml"));
+});
 builder.Services.AddResponseCompression(options => options.EnableForHttps = true);
 builder.Services.AddScoped<IProductByIdQueryHandler, ProductByIdQueryHandler>();
 builder.Services.AddScoped<IValidator<ProductByIdQuery>, ProductByIdQueryValidator>();
