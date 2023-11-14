be6fcc787262eb85ec726fb3b1c02bbb9aa44c6d
Nelson Neto <nelson.ash@outlook.com>
2023-11-14 17:55:21 +0000

    add service validator

--- src/Salddus.Api/Features/ProductByIdQuery.cs
+++ src/Salddus.Api/Features/ProductByIdQuery.cs
@@ -1,10 +1,10 @@
+using Swashbuckle;
+using Swashbuckle.AspNetCore.Swagger;
+using Swashbuckle.AspNetCore.SwaggerGen;
 namespace Salddus.Api.Features;
 
-public record ProductByIdQuery(
-    /// <summary>
-    /// Test
-    /// </summary>
-    /// <example>
-    /// 11
-    /// </example>
-    int Id);
+/// <summary>
+/// The id of the fruit that will be created
+/// </summary>
+/// <param name="Id" example="11"></param>
+public record ProductByIdQuery(int Id);
